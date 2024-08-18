fea04f3b2e85bcb0213f803c37217126fe3acc4d
Dmitry Farino <dfarino46@gmail.com>
2024-08-19 00:38:58 +0300

    Footer and 4 sections done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const menu = document.querySelector('.menu');
+const exchange = document.querySelector('.exchange');
+const tabs = document.querySelectorAll('.tabs');
+const postTabs = document.querySelectorAll('.post-tabs');
 
 if (burgerBtn && menu) {
     burgerBtn.addEventListener('click', () => {
@@ -7,3 +10,57 @@ if (burgerBtn && menu) {
         burgerBtn.classList.toggle('_active');
     });
 }
+
+if (exchange) {
+  const input = exchange.querySelectorAll('.exchange__input input');
+  
+  const INR = 31520; // 1 USD = 31520 INR
+
+  const rounded = (value) => {
+    return +value.toFixed(3);
+  }
+
+  input[0].addEventListener('input', (ev) => {
+    input[1].value = rounded(input[0].value / INR);
+  })
+}
+
+tabs.forEach((tab) => {
+  const tabBtns = tab.querySelectorAll('.tabs__btns .btn');
+
+  tabBtns.forEach((btn, index) => {
+    btn.addEventListener('click', ev => {
+      const activeBtn = tab.querySelector('.tabs__btns .btn._active');
+      const activeContent = tab.querySelector('.tabs__content._active');
+      
+      activeBtn.classList.remove('_active');
+      activeContent.classList.remove('_active');
+
+      const attribute = btn.getAttribute('data-tab');
+      const tabContent = tab.querySelector(`.tabs__content[data-tab="${attribute}"]`);
+
+      btn.classList.add('_active');
+      tabContent.classList.add('_active');
+    });
+  })
+});
+
+postTabs.forEach((tab) => {
+  const tabBtns = tab.querySelectorAll('.post-tabs__btn');
+
+  tabBtns.forEach((btn, index) => {
+    btn.addEventListener('click', ev => {
+      const activeBtn = tab.querySelector('.post-tabs__btn._active');
+      const activeContent = tab.querySelector('.post-tabs__content._active');
+      
+      activeBtn.classList.remove('_active');
+      activeContent.classList.remove('_active');
+
+      const attribute = btn.getAttribute('data-tab');
+      const tabContent = tab.querySelector(`.post-tabs__content[data-tab="${attribute}"]`);
+
+      btn.classList.add('_active');
+      tabContent.classList.add('_active');
+    });
+  })
+});
